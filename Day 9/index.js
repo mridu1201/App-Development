5e5feac7bb857f6ac1f530ad347ff03284344456
Mridini S <110531338+mridu1201@users.noreply.github.com>
2023-09-25 15:28:04 +0530

    Add files via upload

---
+++ Day 9/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.js'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './services/redux/store'
+import { ChakraProvider } from '@chakra-ui/react'
+import { PersistGate } from 'redux-persist/integration/react'
+import DashboardRoutes from './pages/admin/DashboardRoutes.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ChakraProvider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+      {/* <DashboardRoutes/> */}
+    </Provider>
+  </BrowserRouter>
+  </ChakraProvider>
+)
